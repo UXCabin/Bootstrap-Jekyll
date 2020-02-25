a727a18c480b3d39999e06711f7f456a30525199
Seth Coelen <Seth@Coelen.co>
2020-02-25 16:12:06 -0500

    upgrades lodash, ads docs to gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -79,7 +79,7 @@ gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
 gulp.task('browser-sync', ['sass', 'jekyll-build'], function () {
   browserSync({
     server: {
-      baseDir: '_site'
+      baseDir: 'docs'
     }
   });
 });
